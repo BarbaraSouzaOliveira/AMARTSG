20f230363aa4ac87249b4905026e1800c8470eb3
BarbaraSouza <babiholiveira@outlook.com>
2020-10-13 23:12:20 -0300

    first pages configurations

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <index />
+  </div>
+</template>
+
+<script>
+import index from './pages/home/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    index
+  }
+}
+</script>
+
---
+++ src/pages/home/HelloWorld.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <div class="hello">
+   <Navbar />
+   <h1>ola</h1>
+    
+  </div>
+</template>
+
+<script>
+import Navbar from '../../components/Header.vue'
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  components: {
+    Navbar
+  }
+}
+</script>
+
